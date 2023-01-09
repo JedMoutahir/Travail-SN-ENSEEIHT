c31576a0f913287bffcf9208ecb29003876593ae
JedMoutahir <77209356+JedMoutahir@users.noreply.github.com>
2023-01-09 15:01:46 +0100

    Add files via upload

---
+++ 2A/TDL/Projet/passeCodeRatToTam.ml
@@ -0,0 +1,146 @@
+(* Module de la passe de gestion des placements memoire *)
+(* doit être conforme à l'interface Passe *)
+open Tds
+open Ast
+open Type
+open Tam
+open Code
+open Filename
+
+let () = Filename.set_temp_dir_name "/home/jmoutahi/Bureau/2A/TDL/TP02/sourceEtu/tempfortesting"
+
+type t1 = Ast.AstPlacement.programme
+type t2 = string
+
+(* analyse_placement_expression : AstType.expression -> AstPlacement.expression *)
+(* Paramètre e : l'expression à analyser *)
+(* Génere le code pour l'expression de type AstPlacement.expression *)
+let rec generation_code_expression e =
+  match e with
+  | AstType.Ident(info) -> let (dep, base) = 
+  match info_ast_to_info info with
+    | InfoVar(_, _, dep, base) -> (dep, base)
+    | _ -> failwith "Erreur dans generation_code_instruction"
+  in let taille = getTaille (
+    match info_ast_to_info info with
+      | InfoVar(_, t, _, _) ->
+        if (t = Undefined) then failwith "Type Undefined"
+        else t
+      | _ -> failwith "Pas un InfoVar"
+    )
+  in loada dep base
+  ^ loadi taille
+  | AstType.Booleen(b) ->
+    (if b then loadl_int 1
+    else loadl_int 0)
+    ^ subr "I2B"
+  | AstType.Entier(e) -> loadl_int e
+  | AstType.Unaire(op, exp) ->
+    generation_code_expression exp
+    ^ (match op with
+      | AstType.Numerateur -> pop 0 1
+      | AstType.Denominateur -> pop 1 1)
+  | AstType.Binaire(op, eg, ed) -> 
+    generation_code_expression eg
+    ^ generation_code_expression ed
+    ^ (match op with
+      | AstType.PlusInt -> subr "IAdd"
+      | AstType.PlusRat -> call "LB" "RAdd"
+      | AstType.MultInt -> subr "IMul"
+      | AstType.MultRat -> call "LB" "RMul"
+      | AstType.Fraction -> call "LB" "Norm"
+      | AstType.EquBool -> subr "IEq"
+      | AstType.EquInt -> subr "IEq"
+      | AstType.Inf -> subr "ILss")
+  | AstType.AppelFonction(ia, le) -> 
+    let nom = (match info_ast_to_info ia with
+      | InfoFun (n,_,_) -> n
+      | _ -> failwith "Erreur dans generation_code_expression") in
+    String.concat "" (List.map generation_code_expression le)
+    ^ call "LB" nom
+
+(* analyse_placement_instruction : AstType.instruction -> AstPlacement.instruction *)
+(* Paramètre i : l'instruction à analyser *)
+(* Paramètre reg : l'instruction à analyser *)
+(* Paramètre dep : l'instruction à analyser *)
+(* Vérifie la bonne utilisation des types et tranforme l'instruction
+en une instruction de type AstType.instruction *)
+(* Erreur si mauvaise utilisation des types *)
+let rec generation_code_instruction i =
+  match i with
+  | AstPlacement.Affectation (info,e) | AstPlacement.Declaration (info, e) -> let (dep, base) = 
+    match info_ast_to_info info with
+      | InfoVar(_, _, dep, base) -> (dep, base)
+      | _ -> failwith "Erreur dans generation_code_instruction"
+    in let taille = getTaille (
+      match info_ast_to_info info with
+        | InfoVar(_, t, _, _) ->
+          if (t = Undefined) then failwith "Type Undefined"
+          else t
+        | _ -> failwith "Pas un InfoVar"
+      )
+    in generation_code_expression e
+    ^ loada dep base
+    ^ storei taille
+  | AstPlacement.AffichageInt e ->
+    generation_code_expression e
+    ^ subr "IOut"
+  | AstPlacement.AffichageRat e -> 
+    generation_code_expression e
+    ^ call "LB" "ROut"
+  | AstPlacement.AffichageBool e -> 
+    generation_code_expression e
+    ^ subr "BOut"
+  | AstPlacement.Conditionnelle (e,b1,b2) ->
+    let lelse = getEtiquette() in
+    let lend = getEtiquette() in
+    generation_code_expression e
+    ^ jumpif 0 lelse
+    ^ generation_code_bloc b1
+    ^ jump lend
+    ^ label lelse
+    ^ generation_code_bloc b2
+    ^ label lend
+  | AstPlacement.TantQue (e,b) -> 
+    let lloop = getEtiquette() in
+    let lend = getEtiquette() in 
+    label lloop
+    ^ generation_code_expression e
+    ^ jumpif 0 lend
+    ^ generation_code_bloc b
+    ^ jump lloop
+    ^ label lend
+  | AstPlacement.Retour (e, t_ret, param) -> 
+    generation_code_expression e
+    ^ return t_ret param
+  | AstPlacement.Empty -> ""
+
+(* analyse_type_bloc : AstTds.bloc -> AstType.bloc *)
+(* Paramètre li : liste d'instructions à analyser *)
+(* Vérifie la bonne utilisation des types et tranforme le bloc en un bloc de type AstType.bloc *)
+(* Erreur si mauvaise utilisation des types *)
+and generation_code_bloc (li, taille) =
+  push taille
+  ^ String.concat "" (List.map (generation_code_instruction) li)
+  ^ pop 0 taille
+(* analyse_placement_fonction : AstType.fonction -> AstPlacementfonction *)
+(* Paramètre fun : la fonction à analyser *)
+(* Tranforme la fonction en une fonction de type AstPlacement.fonction *)
+let generation_code_fonction (AstPlacement.Fonction (info, _, bloc)) =
+  let (nom, typlist) = (match info_ast_to_info info with
+      | InfoFun (n,_,tl) -> n, tl
+      | _ -> failwith "Erreur dans generation_code_expression") in
+  let taille = List.fold_right (+) (List.map (fun t -> (getTaille t)) typlist) 0 in
+  label nom
+  ^ generation_code_bloc bloc
+  ^ return 0 taille
+
+(* analyser : AstType.programme -> AstPlacement.programme *)
+(* Paramètre : le programme à analyser *)
+(* Tranforme le programme en un programme de type AstPlacement.programme *)
+let analyser (AstPlacement.Programme (fonctions,prog)) =
+  getEntete()
+  ^ String.concat "" (List.map (generation_code_fonction) fonctions)
+  ^ label "main"
+  ^ generation_code_bloc prog 
+  ^ halt
---
+++ 2A/TDL/Projet/passePlacementRat.ml
@@ -0,0 +1,97 @@
+(* Module de la passe de gestion des placements memoire *)
+(* doit être conforme à l'interface Passe *)
+open Tds
+open Exceptions
+open Ast
+open Type
+
+type t1 = Ast.AstType.programme
+type t2 = Ast.AstPlacement.programme
+
+(* analyse_placement_expression : AstType.expression -> AstPlacement.expression *)
+(* Paramètre e : l'expression à analyser *)
+(* Tranforme l'expression en une expression de type AstPlacement.expression *)
+(* Erreur si TODO *)
+let rec analyse_placement_expression e = e
+
+(* analyse_placement_instruction : AstType.instruction -> AstPlacement.instruction *)
+(* Paramètre i : l'instruction à analyser *)
+(* Paramètre reg : l'instruction à analyser *)
+(* Paramètre dep : l'instruction à analyser *)
+(* Vérifie la bonne utilisation des types et tranforme l'instruction
+en une instruction de type AstType.instruction *)
+(* Erreur si mauvaise utilisation des types *)
+let rec analyse_placement_instruction i reg dep =
+  match i with
+  | AstType.Declaration (info, e) ->
+    begin
+      (match (info_ast_to_info info) with
+      | InfoVar(_, t, _, _) -> 
+        (modifier_adresse_variable dep reg info;
+        (AstPlacement.Declaration(info, e), getTaille t))
+      | _ -> failwith "Erreur dans analyse_placement_instruction")
+    end
+  | AstType.Affectation (info,e) ->
+        (AstPlacement.Affectation(info, e), 0)
+  | AstType.AffichageInt e ->
+      (AstPlacement.AffichageInt e, 0)
+  | AstType.AffichageRat e ->
+    (AstPlacement.AffichageRat e, 0)
+  | AstType.AffichageBool e ->
+    (AstPlacement.AffichageBool e, 0)
+  | AstType.Conditionnelle (e,b1,b2) ->
+    let newb1 = analyse_placement_bloc b1 dep reg in
+    let newb2 = analyse_placement_bloc b2 dep reg in
+    (AstPlacement.Conditionnelle(e, newb1, newb2), 0)
+  | AstType.TantQue (e,b) ->
+    let newb = analyse_placement_bloc b dep reg in
+    (AstPlacement.TantQue (e, newb), 0)
+  | AstType.Retour (e, info) ->
+      begin
+        (match (info_ast_to_info info) with
+        | InfoFun(_,typ,lp) -> 
+          (AstPlacement.Retour (e, getTaille typ, List.fold_left (+) 0 (List.map getTaille lp)), 0)
+        | _ -> failwith "Erreur dans analyse_placement_instruction")
+      end
+  | AstType.Empty -> AstPlacement.Empty, 0
+
+(* analyse_placement_bloc : AstType.bloc -> AstPlacement.bloc *)
+(* Paramètre li : liste d'instructions à analyser *)
+(* Paramètre dep *)
+(* Paramètre reg : registre de stockage *)
+(* Tranforme le bloc en un bloc de type AstPlacement.bloc *)
+and analyse_placement_bloc li dep reg =
+match li with
+    | [] -> ([], 0)
+    | i::qi -> let (ni, taille) = (analyse_placement_instruction i reg dep) in 
+    let (nli, tli) = analyse_placement_bloc qi (dep + taille) reg in
+    (ni::nli , taille + tli)
+
+(* analyse_placement_fonction : AstType.fonction -> AstPlacementfonction *)
+(* Paramètre fun : la fonction à analyser *)
+(* Tranforme la fonction en une fonction de type AstPlacement.fonction *)
+let analyse_placement_fonction (AstType.Fonction (info, linfo, bloc)) =
+  let rec modifier_adresse_variable_param li depl =
+    match li with
+    | [] -> ();
+    | t::q -> 
+      match (info_ast_to_info t) with
+      | InfoVar(_,typ,_,_) -> 
+        modifier_adresse_variable (depl-(getTaille typ)) "LB" t;
+        modifier_adresse_variable_param q (depl-(getTaille typ));
+      | _ -> failwith "Internal error"
+  in
+
+  let (nb,tb) = analyse_placement_bloc bloc 3 "LB" in
+  modifier_adresse_variable_param (List.rev linfo) 0;
+
+  AstPlacement.Fonction(info, linfo, (nb, tb))
+
+(* analyser : AstType.programme -> AstPlacement.programme *)
+(* Paramètre : le programme à analyser *)
+(* Tranforme le programme en un programme de type AstPlacement.programme *)
+(* Erreur si TODO *)
+let analyser (AstType.Programme (fonctions,prog)) =
+  let newfun = (List.map analyse_placement_fonction fonctions) in
+  let newbloc = (analyse_placement_bloc prog 0 "SB") in
+  AstPlacement.Programme(newfun, newbloc)
